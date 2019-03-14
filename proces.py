2efea904b7816e6eba2d99de4fda1d1c94150daa
HanlynnKe <mr.kehanlin@outlook.com>
2019-03-14 10:05:06 +0800

    始めのコード

--- proces.py
+++ proces.py
@@ -1,16 +1,21 @@
-import numpy as np
 import cv2 as cv
 
-img = cv.imread("./im/1.bmp")
-img_median = np.hstack([
-    cv.medianBlur(img, 3),
-    cv.medianBlur(img, 5),
-    cv.medianBlur(img, 7)
-])
-cv.imwrite("median_1.jpg", img_median)
-img_gaussian = np.hstack([
-    cv.GaussianBlur(img, (3, 3), 0),
-    cv.GaussianBlur(img, (5, 5), 0),
-    cv.GaussianBlur(img, (7, 7), 0)
-])
-cv.imwrite("gaussian_1.jpg", img_gaussian)
+# 读取图片
+img = cv.imread("./im/7.bmp")
+# 转换为灰度图
+img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+# 使用中值滤波来去除噪声
+img_median = cv.medianBlur(img, 3)
+# 统计图片各个像素灰度值
+grey = []
+for row in img:
+    for px in row:
+        grey.append(px)
+grey.sort()
+# 求灰度值中值用于二值化
+n = len(grey)
+median = grey[int(n / 2)]
+# 二值化
+ret1, img_bi = cv.threshold(img_median, median, 255, cv.THRESH_BINARY)
+# 写出图片
+cv.imwrite("median_bi_median.jpg", img_bi)
