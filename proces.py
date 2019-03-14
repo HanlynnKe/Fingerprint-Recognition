4e453ac0d15aade9ae7d3dc164e2e6bfd003343b
HanlynnKe <mr.kehanlin@outlook.com>
2019-03-14 11:46:55 +0800

    二番目

--- proces.py
+++ proces.py
@@ -1,21 +1,20 @@
+import os
 import cv2 as cv
 
-# 读取图片
-img = cv.imread("./im/7.bmp")
-# 转换为灰度图
-img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
-# 使用中值滤波来去除噪声
-img_median = cv.medianBlur(img, 3)
-# 统计图片各个像素灰度值
-grey = []
-for row in img:
-    for px in row:
-        grey.append(px)
-grey.sort()
-# 求灰度值中值用于二值化
-n = len(grey)
-median = grey[int(n / 2)]
-# 二值化
-ret1, img_bi = cv.threshold(img_median, median, 255, cv.THRESH_BINARY)
-# 写出图片
-cv.imwrite("median_bi_median.jpg", img_bi)
+cwd = os.getcwd()
+file_list = os.listdir(cwd + '/im')
+seq = 1
+for file in file_list:
+    # 读取图片
+    img = cv.imread('./im/' + file)
+    # 转换为灰度图
+    img_gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+    # 使用中值滤波去除噪声
+    img_median = cv.medianBlur(img_gray, 3)
+    # 大津法二值化
+    ret, img_bi = cv.threshold(img_median, 0, 255, cv.THRESH_OTSU)
+    # 再次使用中值滤波去除边缘毛噪和大部分汗孔
+    img_x = cv.medianBlur(img_bi, 5)
+    # 写出图片
+    cv.imwrite('./op/median_bi_' + str(seq) + '.bmp', img_x)
+    seq += 1
