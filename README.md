60e9be3622ae897ad2a635d016f3ce1e4e9a707e
HanlynnKe <mr.kehanlin@outlook.com>
2019-04-08 22:19:39 +0800

    update CODE

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # Fingerprint-Recognition
 
----
-
 This is a ***Digital Image Processing*** project
 
 Fingerprint Recognition based on Python along with OpenCV
 
----
+#### 小心得：
 
-*proces.py* is the most hardcore and simple method and **~~NOT SO~~ SATISFYING**,
+在manual.py中参考国内某篇论文写了个manual_fill的函数来填补汗孔，然而并不是很理想。
+最后在谷歌上面参考着双边滤波的特性（即保持较为明显的边界）结合高斯滤波完成了这次实验。
+希望有时间的话能够研究一下GitHub上面Utkarsh-Deshmutkh的代码仓库。
 
-*skele.py* is aiming at extracting the ridges but **ONLY A FEW** succeeded,
+[https://github.com/Utkarsh-Deshmukh/Fingerprint-Enhancement-Python]
 
-***manual.py*** is the latest version of fingerprint ridge extraction and the **TESTING** one.
+我仓库里skeleton.py是一点骨架提取的代码，前期有点帮助，后来还是舍弃了这种办法。
+助教不是很给力，寻求帮助的时候一点有用的提示都没有 :-(
--- manual.py
+++ manual.py
@@ -1,5 +1,14 @@
 import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
 import os
+
+# cwd = os.getcwd()
+# file_list = os.listdir(cwd + '/im')
+file_list = ['4.bmp', '14.bmp', '24.bmp']
+
+
+# file_list = ['4.bmp']
 
 
 # 填补汗孔函数
@@ -49,41 +58,65 @@ def manual_fill(img, iteration, threshold):
                         w_count += 1
                     if img[y + 1][x - 1] == 0:
                         b_count += 1
+                        # w_count -= 1
                     else:
                         # b_count -= 1
                         w_count += 1
                     if img[y][x - 1] == 0:
                         b_count += 1
+                        # w_count -= 1
                     else:
                         # b_count -= 1
                         w_count += 1
-                    if b_count >= threshold:
+                    if b_count > threshold:
                         img[y][x] = 0
-                    if w_count >= threshold:
+                    if w_count > threshold:
                         img[y][x] = 255
                     b_count = 0
                     w_count = 0
     return img
 
 
-# cwd = os.getcwd()
-# file_list = os.listdir(cwd + '/im')
-file_list = ['4.bmp', '14.bmp', '24.bmp']
-# file_list = ['24.bmp']
+def enhance_contrast(src, a, b):
+    i_height = src.shape[0]
+    i_width = src.shape[1]
+    blank = np.zeros([i_height, i_width], src.dtype)
+    dst = cv.addWeighted(src, a, blank, 1 - a, b)
+    return dst
+
+
+def show_hist(src):
+    # 图像直方图
+    hist = cv.calcHist(src, [0], None, [256], [0, 256])
+    plt.figure()  # 新建一个图像
+    plt.title("Grayscale Histogram")  # 图像的标题
+    plt.xlabel("Bins")  # X轴标签
+    plt.ylabel("# of Pixels")  # Y轴标签
+    plt.plot(hist)  # 画图
+    plt.xlim([0, 256])  # 设置x坐标轴范围
+    plt.show()  # 显示图像
+
+
 for file in file_list:
     # 读取图片
     img = cv.imread('./im/' + file, 0)
     # cv.imshow('origin', img)
-    # 中值滤波去噪
-    img = cv.medianBlur(img, 3)
-    # 直方图均衡化
+    # # 中值滤波去噪
+    # img = cv.medianBlur(img, 5)
+    # cv.imshow('medi', img)
     img = cv.equalizeHist(img)
-    # cv.imshow('hist', img)
+    img = cv.bilateralFilter(img, 9, 75, 75)
+    # cv.imshow('fil', blur)
+    # # 提高对比度和亮度
+    # blur = enhance_contrast(blur, 0.8, 50)
+    # cv.imshow('con', blur)
     # 二值化
-    ret, img = cv.threshold(img, 180, 255, cv.THRESH_OTSU)
-    # cv.imshow('bi', img)
-    # 手动填补
-    img = manual_fill(img, 6, 5)
+    img = cv.GaussianBlur(img, (5, 5), 0)
+    # cv.imshow('g', blur)
+    ret1, img = cv.threshold(img, 180, 255, cv.THRESH_OTSU)
+    # cv.imshow('bi1', blur)
+    # # 手动填补
+    # img = manual_fill(blur, 10, 4)
     # cv.imshow('pores', img)
     # 写出图片
     cv.imwrite('./op/ridges_' + file, img)
