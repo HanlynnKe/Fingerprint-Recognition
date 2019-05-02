bf6d835de124b3d50680950e6c14c88fb70826b7
HanlynnKe <mr.kehanlin@outlook.com>
2019-05-02 15:11:51 +0800

    update code

--- manual.py
+++ manual.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import numpy as np
-import os
+# import os
 
 # cwd = os.getcwd()
 # file_list = os.listdir(cwd + '/im')
@@ -9,27 +9,28 @@ file_list = ['4.bmp']
 
 for file in file_list:
     # 读取图片
-    img = cv.imread('./im/' + file, 0)
-    cv.imshow('origin', img)
+    origin = cv.imread('./im/' + file, 0)
+    # cv.imshow('origin', img)
     # 去噪
-    img = cv.medianBlur(img, 5)
+    img = cv.medianBlur(origin, 5)
     img = cv.equalizeHist(img)
     img = cv.bilateralFilter(img, 9, 75, 75)
-    cv.imshow('fil', img)
-    # 提高对比度和亮度
+    # cv.imshow('fil', img)
+    # 改善对比度和亮度
     for y in range(img.shape[0]):
         for x in range(img.shape[1]):
             img[y, x] = np.clip(0.9 * img[y, x] + (-100), 0, 255)
-    cv.imshow('light', img)
+    # cv.imshow('light', img)
     cl = cv.createCLAHE(clipLimit=30, tileGridSize=(8, 8))
     img = cl.apply(img)
-    cv.imshow('output', img)
+    # cv.imshow('output', img)
     # 二值化
     img = cv.GaussianBlur(img, (3, 3), 0)
     ret1, img = cv.threshold(img, 50, 255, cv.THRESH_OTSU)
-    cv.imshow('bi1', img)
-    # # 写出图片
-    # cv.imwrite('./op/ridges_' + file, img)
+    # cv.imshow('bi1', img)
+    # 写出图片
+    cv.imwrite('./op/ridges_' + file, img)
+    cv.imshow('before & after', np.hstack([origin, img]))
     # 结束展示
     cv.waitKey(0)
     cv.destroyAllWindows()
